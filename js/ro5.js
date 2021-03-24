1758ac195ec06e86a4295bde09dbff76586cff59
TarasSheva <tarasgsheva@gmail.com>
2021-03-24 18:17:12 +0200

    first commit

---
+++ js/ro5.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+
+    window.countryList = {
+        ro: {
+            name: 'România',
+            code: 'ro',
+            price: 159,
+            oldPrice: 318,
+            labelPrice: ' RON',
+            phoneHelper: 'De exemplu, +40 123456789 ',
+            nameHelper: 'Ion Enache',
+            nameError: 'Este necesar să indicaţi numele',
+            phoneError: 'Este necesar să indicaţi numărul de telefon',
+            countryError: 'Este necesar să indicaţi ţara',
+            isDefault: true
+        }
+    };
+    
+})();
